145203f3254206c00dd866d8cb3ea402668cc507
jvila42 <VilaJ96@gmail.com>
2016-02-04 10:29:04 -0500

    Create February-4.R

---
+++ February-4.R
@@ -0,0 +1,23 @@
+#  February 4
+
+#  We will begin by talking about different
+#  types of files, and their uses
+
+#  Reading files using a path statement
+#  instead of file.choose().
+#  R wants only forward slashes, so if
+#  your path has backward ones, change them.
+
+#  Try reading in the groceries data again:
+#  G <- read.csv("  path statement ")
+
+
+#  
+#  Next, we created an R markdown file 
+#  We started by clicking File, then
+#  New File, then R Markdown.  Then select
+#  From Template (on the left), and choose
+#  the DataComputing Simple Template.  
+
+#  Edit the template to look like the 
+#  file  practice.Rmd (also attached)
